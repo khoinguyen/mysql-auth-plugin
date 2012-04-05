198f939e26713d3f2aab7a2c11a3b7ce9eccb459
Khoi Nguyen <khoi.nqq@gmail.com>
2012-04-06 02:38:44 +0700

    MessageDigest.toString() do not return an digested string but something like SHA-1 from Sun... use MessageDigest.digest and convert from Bytes to Hex string instead

--- src/main/java/hudson/plugins/mysql/crypt/Cipher.java
+++ src/main/java/hudson/plugins/mysql/crypt/Cipher.java
@@ -79,7 +79,7 @@ public class Cipher
                 md.reset();
                 byte[] textBytes = plaintext.getBytes();
                 md.update(textBytes);
-                return md.toString();
+                return bytesToHex(md.digest());
             }
             catch (NoSuchAlgorithmException ex)
             {
@@ -90,4 +90,16 @@ public class Cipher
 
     private String encryptionMethod;
     private String salt;
+
+    public static String bytesToHex(byte[] b) {
+      char hexDigit[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                         '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+      StringBuffer buf = new StringBuffer();
+      for (int j=0; j<b.length; j++) {
+         buf.append(hexDigit[(b[j] >> 4) & 0x0f]);
+         buf.append(hexDigit[b[j] & 0x0f]);
+      }
+      return buf.toString();
+   }
+
 }
